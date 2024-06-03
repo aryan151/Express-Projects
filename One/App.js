15a23fc8704679b8e9c034b6d741e61a41b99461
Aryan Azar <74626363+aryan151@users.noreply.github.com>
2024-06-02 21:08:27 -0400

    os part 1

--- One/App.js
+++ One/App.js
@@ -1,13 +1,7 @@
-const {john, peter} = require('./4-names') 
-const sayHi = require('./5-utils')
-const data = require('./6-alternative-flavor')    
-require('./7-mind-grenade')  //function inside module will run   
+const os = require('os')
 
 
-// sayHi('susan')
-// sayHi(john) // sayHi(names.john)
-// sayHi(peter)    //sayHi(names.peter)    
-
-
-
-//import module imports functions as well   
+//info about corrent user 
+     
+const user = os.userInfo() 
+console.log(user)
