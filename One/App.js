afd0528e88ea7e236e18ce69123073764e497025
Aryan Azar <74626363+aryan151@users.noreply.github.com>
2024-06-02 20:46:13 -0400

    module exports

--- One/App.js
+++ One/App.js
@@ -1,8 +1,13 @@
 const {john, peter} = require('./4-names') 
 const sayHi = require('./5-utils')
-    
-   
+const data = require('./6-alternative-flavor')    
+require('./7-mind-grenade')  //function inside module will run   
 
-sayHi('susan')
-sayHi(john) // sayHi(names.john)
-sayHi(peter)    //sayHi(names.peter)
+
+// sayHi('susan')
+// sayHi(john) // sayHi(names.john)
+// sayHi(peter)    //sayHi(names.peter)    
+
+
+
+//import module imports functions as well 
