64cc19c3ec47463ee72558f32ac7ace8eebc1946
Aryan Azar <74626363+aryan151@users.noreply.github.com>
2024-06-21 16:20:55 -0400

    Update 6-alternative-flavor.js

--- One/6-alternative-flavor.js
+++ One/6-alternative-flavor.js
@@ -2,5 +2,11 @@ module.exports.items = ['item1', 'item2']
 const person = {
     name: 'bob'
 }
+const person2 = {
+    name: 'bob2'
+}
+const person3 = {
+    name: 'bob3'     
+}
 
 module.exports.singlePerson = person    
