3be16adab0f4d6f83345eca6be9260436b48e241
Aryan Azar <74626363+aryan151@users.noreply.github.com>
2024-06-21 16:20:22 -0400

    Update App.js

--- One/App.js
+++ One/App.js
@@ -5,13 +5,13 @@ const os = require('os')
      
 const user = os.userInfo() 
 //returns system uptime in seconds 
-console.log(user)  
+// console.log(user)  
                
 
-console.log(`The System Uptime is ${os.uptime} seconds`)
+// console.log(`The System Uptime is ${os.uptime} seconds`)
              
 const currentOS = {
-    name:os.type(),
+    name:os.type(),  
     release:os.release(),
     totalMem: os.totalmem(),
     freeMem: os.freemem(),
