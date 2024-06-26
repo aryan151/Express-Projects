5ae7bbe823a68aa13ba6920510f5ec111444c868
Aryan Azar <74626363+aryan151@users.noreply.github.com>
2024-06-26 14:52:06 -0400

    path module

---
+++ One/8-os-module.js
@@ -0,0 +1,20 @@
+const os = require('os')
+
+         
+//info about corrent user          
+     
+const user = os.userInfo() 
+//returns system uptime in seconds 
+// console.log(user)  
+               
+
+// console.log(`The System Uptime is ${os.uptime} seconds`)
+             
+const currentOS = {
+    name:os.type(),  
+    release:os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
+
+}  
+console.log(currentOS) 
